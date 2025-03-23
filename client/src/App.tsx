63b49fb37ef6fa894f1bd275db1d3c3321098d95
LDegraz <degrazial1@yahoo.com>
2025-03-23 19:06:17 -0400

    fixed code based on errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,44 @@
+// src/App.tsx
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Create an HTTP link to the GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Add the JWT token to each request if available
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create the Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,101 @@
+import { UserInputError, AuthenticationError } from 'apollo-server-express';
+import User from '../models/User.js';
+import { signToken } from '../services/auth.js';
+import type { Resolvers } from '@apollo/server-express';
+import type { Context } from '../types/context';
+import type { UserDocument } from '../models/User';
+
+interface MeArgs {}
+
+interface LoginArgs {
+    email: string;
+    password: string;
+}
+
+interface AddUserArgs {
+    username: string;
+    email: string;
+    password: string;
+}
+
+interface SaveBookArgs {
+    input: {
+        bookId: string;
+        authors: string[];
+        description: string;
+        title: string;
+        image: string;
+        link: string;
+    };
+}
+
+interface RemoveBookArgs {
+    bookId: string;
+}
+
+const resolvers: Resolvers = {
+    Query: {
+        me: async (_parent: undefined, _args: MeArgs, context: Context): Promise<UserDocument | null> => {
+            if (!context.user) {
+                throw new AuthenticationError('You must be logged in');
+            }
+
+            const foundUser = await User.findById(context.user._id);
+            return foundUser;
+        },
+    },
+
+    Mutation: {
+        login: async (_parent: undefined, { email, password }: LoginArgs): Promise<{ token: string; user: UserDocument }> => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new UserInputError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password');
+            }
+
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+
+        addUser: async (_parent: undefined, args: AddUserArgs): Promise<{ token: string; user: UserDocument }> => {
+            const user = await User.create(args);
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+
+        saveBook: async (_parent: undefined, { input }: SaveBookArgs, context: Context): Promise<UserDocument | null> => {
+            if (!context.user) {
+                throw new AuthenticationError('You must be logged in');
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(
+                context.user._id,
+                { $addToSet: { savedBooks: input } },
+                { new: true, runValidators: true }
+            );
+
+            return updatedUser;
+        },
+
+        removeBook: async (_parent: undefined, { bookId }: RemoveBookArgs, context: Context): Promise<UserDocument | null> => {
+            if (!context.user) {
+                throw new AuthenticationError('You must be logged in');
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(
+                context.user._id,
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+
+            return updatedUser;
+        },
+    },
+};
+
+export default resolvers;
